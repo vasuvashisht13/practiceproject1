c683025f0642e814f85c9edf4ccb372f6d4b3b29
Vasu <vasuvashisht0001@gmail.com>
2025-04-15 14:19:44 +0530

    new changed in put file

--- tests/put.spec.ts
+++ tests/put.spec.ts
@@ -1,4 +1,5 @@
 import { test, expect } from '@playwright/test';
+import { json } from 'stream/consumers';
 
 test('api testing put request', async ({ request }) => {
     const respPut = await request.put("https://restful-booker.herokuapp.com/booking/1", {
@@ -22,4 +23,31 @@ test('api testing put request', async ({ request }) => {
     expect(respPut.status()).toBe(200);
     const jsonresp = await respPut.json();
     console.log(jsonresp);
+    expect(respPut.ok()).toBeTruthy();
+    expect(jsonresp).toMatchObject({
+        firstname: 'Vasu',
+        lastname: 'Vashisht',
+        totalprice: 999,
+        depositpaid: true,
+        bookingdates: { checkin: '2018-01-01', checkout: '2019-01-01' },
+        additionalneeds: 'Breakfast'
+      })
+
+      expect(jsonresp.additionalneeds).toEqual('Breakfast');
+
 });
+
+test('api get request to verify data', async({request}) =>{
+    const respget = await request.get('https://restful-booker.herokuapp.com/booking/1');
+    const jsondata = await respget.json();
+    console.log(jsondata);
+
+    expect(jsondata).toMatchObject({
+        firstname: 'Vasu',
+        lastname: 'Vashisht',
+        totalprice: 999,
+        depositpaid: true,
+        bookingdates: { checkin: '2018-01-01', checkout: '2019-01-01' },
+        additionalneeds: 'Breakfast'
+      })
+})
